6cdcf3e01b7ce8a3b9de4f84d6c3680aab99e5ea
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2024-02-22 10:49:50 +0530

    added login feature

--- website/views.py
+++ website/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    #check to see if loged in
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['password']
+        # authenticate user
+        user = authenticate(request, username=username, password=passwd)
+        if user is not None:
+            login(request,user)
+            messages.success(request,('You have been logged in'))
+            return redirect('home')
+        else:
+            messages.success(request,('Error logging in - Please try again'))
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
 
 
 def logout_user(request):
